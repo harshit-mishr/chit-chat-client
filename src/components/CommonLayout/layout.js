f578216d8709e920b8a96ee0f47213164af7c619
MD ASHARUDDIN <119596353+MD-ASHARUDDIN-90@users.noreply.github.com>
2024-05-07 23:36:28 +0530

    Navbar sidebar component (#8)
    * Separate Navbar into components
    
    * Fix sidebar and separate components

---
+++ src/components/CommonLayout/layout.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Navbar from '../Navbar';
+import { Layout } from 'antd';
+import apiService from '@/service/apiService';
+import { useRouter } from 'next/navigation';
+import SideBar from '../Sidebar';
+
+function MainLayout({ children, collapsed, setCollapsed }) {
+    const [userData, setUserData] = useState(null);
+    const router = useRouter();
+
+    const getUserData = async () => {
+        try {
+            const response = await apiService.get('/user', false);
+            console.log('response', response);
+            setUserData(response.data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    React.useEffect(() => {
+        getUserData();
+    }, []);
+
+    const logout = async () => {
+        try {
+            const refreshToken = localStorage.getItem('refreshToken');
+            const response = await apiService.post(
+                '/auth/logout',
+                { refreshToken },
+                false,
+            );
+            console.log('response', response);
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('refreshToken');
+            router.push('/auth/login');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        <Layout>
+            <Navbar
+                collapsed={collapsed}
+                setCollapsed={setCollapsed}
+                userData={userData}
+                logout={logout}
+            />
+            <Layout>
+                {
+                    //     <Sider
+                    //     width={'20rem'}
+                    //     style={{
+                    //         overflow: 'auto',
+                    //         height: '100vh',
+                    //         position: 'fixed',
+                    //         left: 0,
+                    //         top: '5rem',
+                    //         bottom: 0,
+                    //         transition: 'margin-left 0.3s ease-in-out',
+                    //     }}
+                    //     // collapsible
+                    //     collapsed={collapsed}
+                    //     onCollapse={value => setCollapsed(value)}
+                    // >
+                    //     <Menu
+                    //         theme="dark"
+                    //         defaultSelectedKeys={['1']}
+                    //         mode="inline"
+                    //         items={items}
+                    //     />
+                    // </Sider>
+                }
+                <SideBar collapsed={collapsed} setCollapsed={setCollapsed} />
+
+                {children}
+            </Layout>
+        </Layout>
+    );
+}
+
+export default MainLayout;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,251 @@
+import { Avatar, Badge, Input, Layout, Menu, Typography } from 'antd';
+import React, { useState } from 'react';
+import {
+    UserOutlined,
+    MenuUnfoldOutlined,
+    AudioOutlined,
+    MenuFoldOutlined,
+    HomeOutlined,
+    ContactsOutlined,
+    UsergroupAddOutlined,
+    BellOutlined,
+} from '@ant-design/icons';
+import CustomPopover from '../CustomPopover';
+
+const { Header } = Layout;
+const { Title } = Typography;
+const { Search } = Input;
+
+function Navbar({ collapsed, setCollapsed, userData, logout }) {
+    const onSearch = (value, _e, info) => console.log(info?.source, value); //will update in future
+    const [selectedKey, setSelectedKey] = useState('1');
+    const handleMenuClick = e => {
+        setSelectedKey(e.key);
+    };
+
+    const navbarItems = [
+        {
+            key: '1',
+            label: (
+                <HomeOutlined
+                    style={{
+                        fontSize: '1.2rem',
+                        color:
+                            selectedKey === '1'
+                                ? '#1668dc'
+                                : 'rgba(255, 236, 236, 0.726)',
+                    }}
+                />
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <UsergroupAddOutlined
+                    style={{
+                        fontSize: '1.2rem',
+                        color:
+                            selectedKey === '2'
+                                ? '#1668dc'
+                                : 'rgba(255, 236, 236, 0.726)',
+                    }}
+                />
+            ),
+        },
+        {
+            key: '3',
+            label: (
+                <ContactsOutlined
+                    style={{
+                        fontSize: '1.2rem',
+                        color:
+                            selectedKey === '3'
+                                ? '#1668dc'
+                                : 'rgba(255, 236, 236, 0.726)',
+                    }}
+                />
+            ),
+        },
+    ];
+
+    const navbarSecondaryItems = [
+        {
+            key: '4',
+            label: (
+                <UserOutlined
+                    style={{
+                        fontSize: '1.2rem',
+                        color: 'rgba(255, 236, 236, 0.726)',
+                    }}
+                />
+            ),
+        },
+        {
+            key: '5',
+            label: (
+                <BellOutlined
+                    style={{
+                        fontSize: '1.2rem',
+                        color: 'rgba(255, 236, 236, 0.726)',
+                    }}
+                />
+            ),
+        },
+    ];
+
+    return (
+        <Header
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                height: '5rem',
+                paddingLeft: '1.5rem',
+                paddingRight: '1.5rem',
+                position: 'fixed',
+                width: '100%',
+                top: 0,
+                // border: '1px solid #fff',
+            }}
+        >
+            <div
+                style={{
+                    // border: '1px solid black',
+                    display: 'flex',
+                    alignItems: 'center',
+                    // flex: 0.2,
+                    width: '20%',
+                }}
+            >
+                {collapsed ? (
+                    <MenuUnfoldOutlined
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '1.5rem',
+                            color: 'rgba(255, 236, 236, 0.726)',
+                        }}
+                    />
+                ) : (
+                    <MenuFoldOutlined
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '1.5rem',
+                            color: 'rgba(255, 236, 236, 0.726)',
+                        }}
+                    />
+                )}
+
+                <Title
+                    style={{
+                        // flex: 0.3,
+                        marginLeft: '3vh',
+                        textDecoration: 'underline',
+                    }}
+                    level={4}
+                >
+                    ChitChat
+                </Title>
+            </div>
+            <div
+                style={{
+                    // flex: 1,
+                    width: '80%',
+                    // border: '1px solid red',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}
+            >
+                <Search
+                    placeholder="input search text"
+                    enterButton="Search"
+                    size="middle"
+                    style={{ width: '25vw' }}
+                    suffix={
+                        <AudioOutlined
+                            style={{
+                                fontSize: 16,
+                                color: '#1677ff',
+                            }}
+                        />
+                    }
+                    onSearch={onSearch}
+                />
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '20%',
+                        // border: '1px solid red',
+                        width: '15vw',
+                    }}
+                >
+                    {navbarItems.map(item => (
+                        <div
+                            onClick={() => handleMenuClick(item)}
+                            key={item.key}
+                            style={{
+                                borderBottom:
+                                    item.key === selectedKey
+                                        ? '2px solid #1668dc'
+                                        : 'none',
+                                backgroundColor:
+                                    item.key === selectedKey
+                                        ? 'transparent'
+                                        : 'inherit',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            <Avatar
+                                style={{
+                                    backgroundColor: '#141414',
+                                    border: '1px solid #424242',
+                                    borderColor:
+                                        item.key === selectedKey
+                                            ? '#1668dc'
+                                            : '#424242',
+                                }}
+                                shape="round"
+                                size="large"
+                            >
+                                {item.label}
+                            </Avatar>
+                        </div>
+                    ))}
+                </div>
+
+                <div
+                    style={{
+                        // border: '1px solid red',
+                        width: '10vw',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignContent: 'center',
+                        gap: '10%',
+                    }}
+                >
+                    <Badge count={5}>
+                        <Avatar
+                            style={{
+                                backgroundColor: '#141414',
+                                border: '1px solid #424242',
+                                borderColor: '#424242',
+                            }}
+                            shape="round"
+                            size="large"
+                        >
+                            <BellOutlined
+                                style={{
+                                    fontSize: '1.2rem',
+                                    color: 'rgba(255, 236, 236, 0.726)',
+                                }}
+                            />
+                        </Avatar>
+                    </Badge>
+                    <CustomPopover userData={userData} logout={logout} />
+                </div>
+            </div>
+        </Header>
+    );
+}
+
+export default Navbar;
